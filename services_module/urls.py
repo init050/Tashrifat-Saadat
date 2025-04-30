3dfb8954f210dedc13e4ee065f14f35afcfd2550
init050 <fazelzare@proton.me>
2025-04-30 04:36:22 +0330

    Refactor service detail URL handling to use slugs instead of primary keys, updating links in home and service pages accordingly for improved readability and SEO.

--- services_module/urls.py
+++ services_module/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ServiceListView.as_view(), name='service_page'),
-    path('<pk>/', views.ServiceDetailView.as_view(), name = 'service_detail_page'),
+    path('<slug:slug>/', views.ServiceDetailView.as_view(), name = 'service_detail_page'),
 ]
